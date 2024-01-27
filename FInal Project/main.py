5252420d40cefdd46840bd820a96aea1e07d5768
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-01-27 20:06:15 +0530

    trained model and added computer vision

--- FInal Project/main.py
+++ FInal Project/main.py
@@ -1,26 +1,39 @@
 import cv2 as cv    
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import tensorflow as tf
 
-mnist = tf.keras.dataset.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# mnist = tf.keras.datasets.mnist
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_train, axis=1)
+# x_train = tf.keras.utils.normalize(x_train, axis=1)
+# x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.Layers.Flatten(input_shape=(28,28)))
-model.add(tf.keras.Layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.Layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.Layers.Dense(10, activation=tf.nn.softmax))  
+# model = tf.keras.models.Sequential()
+# model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+# model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+# model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+# model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))  
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# model.compile(
+#             optimizer='adam', 
+#             loss='sparse_categorical_crossentropy',
+#             metrics=['accuracy']
+#             )
 
-model.fit(x_train, y_train, epochs=3)
+# model.fit(x_train, y_train, epochs=3)
 
-accuracy, loss = model.evaluate(x_test, y_test)
-print(accuracy)
-print(loss)
+# loss, accuracy = model.evaluate(x_test, y_test)
+# print(accuracy)
+# print(loss)
 
-model.save('digits.model')
+# print(len(x_test))
+# print(len(y_test))
+
+# model.save('digits.model')
+
+for x in range(1,2):
+    img = cv.imread(f'{x}.png')[:,:,0]
+    img = np.array([img])
+    plt.imshow(img[0])
+    plt.show()
