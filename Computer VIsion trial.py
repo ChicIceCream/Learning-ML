78b4883d677497fa63106d775df2a7b74845de2d
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-02-02 00:32:14 +0630

    improved

--- Computer VIsion trial.py
+++ Computer VIsion trial.py
@@ -8,8 +8,7 @@ fashion_mnist = keras.datasets.fashion_mnist
 
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(256, activation='relu'),
-    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(128, activation='relu'),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
