56c3d45da6d91b0a735ed4a740d21fa551816bb5
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-02-02 18:16:21 +0630

    added some more

--- Computer VIsion trial.py
+++ Computer VIsion trial.py
@@ -1,10 +1,14 @@
 import tensorflow as tf 
 from tensorflow import keras
-
+import matplotlib.pyplot as plt
 
 fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+(training_images, training_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+
+plt.imshow(training_images[0])
+print(training_labels[0])
+print(training_images[0])
 
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
@@ -18,9 +22,11 @@ model.compile(
     metrics = ['accuracy'],
 )
 
-model.fit(train_images, train_labels, epochs=3)
+model.fit(training_images, training_labels, epochs=5)
 
 test_loss, test_accuracy = model.evaluate(test_images, test_labels)
-print(test_loss, test_accuracy)
+# print(test_loss, test_accuracy)
 
+classifications = model.predict(test_images)
+print(classifications[0])  
 
