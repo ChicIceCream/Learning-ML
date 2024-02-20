4ff067a09f71730362a896bed29f2a23221fb78c
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-02-21 01:19:47 +0530

    done more

--- OpenCV/draw.py
+++ OpenCV/draw.py
@@ -16,7 +16,28 @@ blank = np.zeros((500,500,3), dtype='uint8')
 # resized_img = rescaleFrame(img)
 # cv.imshow('Cat', resized_img)
 
-blank[200:300, 300:400] = 0,255,0
+# 1. Paint whole screen a colour
+blank[:] = 0,255,0
 cv.imshow('Green Screen', blank)
+
+# 2. Only painitng a little place
+blank[200:300, 300:400] = 0,0,255
+cv.imshow('Green Screen with squre', blank)
+
+# 3. Drawing a rectangle
+cv.rectangle(blank, (0,0), (250,500), (255,0,0), thickness=-1) #! use thinkness=-1 to fill whole
+cv.imshow('Rectanlge', blank)
+
+# 4. drawing a circle
+cv.circle(blank, (250,250), 40, (255,255,255), thickness=4)
+cv.imshow('Cricle', blank)
+
+# 5. Drawing a line
+cv.line(blank, (50,250), (450,250), (0,0,0) , thickness=6)
+cv.imshow("Line", blank)
+
+# 6. Wrtiting text in the image
+cv.putText(blank, 'Hallooo', (100,100), cv.FONT_HERSHEY_TRIPLEX, 1.0, (112,124,9), thickness=4)
+cv.imshow('Text', blank)
 
 cv.waitKey(0)
