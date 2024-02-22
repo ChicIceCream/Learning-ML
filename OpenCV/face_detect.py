6ed6aa69aaa0980c2cc250cef1e725f98f401ec3
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-02-23 02:52:49 +0530

    finished haar cascade

--- OpenCV/face_detect.py
+++ OpenCV/face_detect.py
@@ -1,10 +1,18 @@
 import cv2 as cv
 
-img = cv.imread('OpenCV/Photos/lady.jpg')
-cv.imshow('Person_Lady', img)
+def rescaleFrame(frame, scale=1.3):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    
+    dimensions = (width, height)
+    
+    return cv.resize(img, dimensions, interpolation=cv.INTER_AREA)
+
+img = cv.imread('OpenCV/Photos/group 1.jpg')
+# cv.imshow('group', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
+# cv.imshow('Gray', gray)
 
 # Load the Haar cascade classifier XML file
 haar_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -16,8 +24,14 @@ else:
     print("Haar cascade classifier XML file loaded successfully.")
 
 # Detect faces in the image
-faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
 
 print(f'Number of faces found = {len(faces_rect)}')
+
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img, (x,y), (x+w, y+h), (0,255,0), thickness=1)
+
+img = rescaleFrame(img)
+cv.imshow('Detected Faces', img)
 
 cv.waitKey(0)
