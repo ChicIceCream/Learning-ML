8804215f0986c41b8c0daf2864b38a64543bd73a
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-02-26 04:34:58 +0530

    tried again

--- OpenCV/face_recognition.py
+++ OpenCV/face_recognition.py
@@ -1,11 +1,12 @@
 import numpy as np
 import cv2 as cv
 
-def rescaleFrame(frame, scale):
-    width = int(frame.shape[1] * scale)
-    height = int(frame.shape[0] * scale)
+def rescaleFrame(frame, scale=1.5):
+    width = int(frame[1] * scale)
+    height = int(frame[0] * scale)
+    
     dimensions = (width, height)
-
+    
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
 haar_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
