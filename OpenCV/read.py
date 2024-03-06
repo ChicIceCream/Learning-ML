3df03c58f9e0dde15cb1ba915c89936f5139ec99
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-03-06 23:50:21 +0530

    changed to camera

--- OpenCV/read.py
+++ OpenCV/read.py
@@ -1,19 +1,19 @@
 import cv2 as cv
 
-# capture = cv.VideoCapture('OpenCV\Videos\dog.mp4') #! put 0 for laptop camera
+capture = cv.VideoCapture(0) #! put 0 for laptop camera
 
-# while True:
-#     isTrue, frame = capture.read()
-#     cv.imshow('Video of dog', frame)
-    
-#     if cv.waitKey(20) & 0xFF==ord('d'):
-#         break
-# capture.release()
-# cv.destroyAllWindows()
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video of dog', frame)
 
-img = cv.imread('OpenCV\Photos\cat.jpg')
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
 
-cv.imshow("Cat", img)
+# img = cv.imread('OpenCV\Photos\cat.jpg')
+
+# cv.imshow("Cat", img)
 
 
 cv.waitKey(0)
