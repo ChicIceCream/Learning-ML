aeed9e8996c491d4f0d304759f1d188d6a46e4fa
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-04-26 18:52:47 +0530

    wanted to try smth

--- K-Nearest Neighbors/main.py
+++ K-Nearest Neighbors/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 
@@ -10,7 +11,8 @@ print(data.target_names)
 
 X_train, X_test, Y_train, Y_test = train_test_split(np.array(data.data), np.array(data.target), test_size=0.2)
 
-clf = KNeighborsClassifier(n_neighbors=3)
+# clf = KNeighborsClassifier(n_neighbors=3)
+clf = LogisticRegression()
 clf.fit(X_train, Y_train)
 
 print(clf.score(X_test, Y_test)*100)
