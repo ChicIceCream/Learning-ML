ae1adc451e63332c3e00fd03b656ec8039776825
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-05-03 16:33:42 +0530

    Decision Tree Regressor

--- Decision Trees + Random Forest/Decision_Tree_Regressor.py
+++ Decision Trees + Random Forest/Decision_Tree_Regressor.py
@@ -3,8 +3,15 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 iris = load_iris()
 # print(iris) 
 # print(iris.data) 
+
+classifier = DecisionTreeClassifier()
+classifier.fit(iris.data, iris.target)
+
+plt.figure(figsize=(10,10))
+tree.plot_tree(classifier, filled=True)
+plt.show()
