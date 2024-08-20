780b9c84d6c80af5635c75db5ed5bcd89b760f34
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-08-20 17:51:02 +0530

    building chatbot

---
+++ ChatBot_Sentdex/main.py
@@ -0,0 +1,36 @@
+import sqlite3
+import json
+from datetime import datetime
+
+timeframe = '2015-05'
+sql_transaction = []
+
+connection = sqlite3.connect('{}.db'.format(timeframe))
+c = connection.cursor()
+
+def create_table():
+    c.execute(
+    """
+    CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, comment_id, 
+    TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unit INT, score INT)
+    """
+    )
+
+def format_data(data):
+    data = data.replace("\n", " newlinechar ")
+
+if __name__ == "__main__":
+    create_table()
+    rows_counter = 0
+    paired_rows = 0
+    
+    with open("BLAH".format(timeframe.split('-')[0], timeframe), buffer=1000) as f:
+        for row in f:
+            rows_counter += 1
+            row = json.load(row)
+            parnet_id = row['parent_id']
+            body = format_data(row['body'])
+            created_utc = row['created_utc']
+            score = row['score']
+            subreddit = row['subreddit']
+            
