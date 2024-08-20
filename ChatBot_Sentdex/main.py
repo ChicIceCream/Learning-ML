2d6a7c6255bb1ad14b788f6d54bb85bc11152572
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-08-20 19:11:22 +0530

    working with database

--- ChatBot_Sentdex/main.py
+++ ChatBot_Sentdex/main.py
@@ -17,15 +17,30 @@ def create_table():
     )
 
 def format_data(data):
-    data = data.replace("\n", " newlinechar ")
+    data = data.replace("\n", " newlinechar ").replace("\r", " newlinechar ").replace('"', "'")
+    return data
+
+def find_parent(pid):
+    try:
+        sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+    except Exception as e:
+        print("find_parent {e}")
+
 
 if __name__ == "__main__":
     create_table()
     rows_counter = 0
     paired_rows = 0
     
-    with open("BLAH".format(timeframe.split('-')[0], timeframe), buffer=1000) as f:
+    with open("BLAH".format(timeframe.split('-')[0], timeframe), buffering=1000) as f:
         for row in f:
+            print(row)
             rows_counter += 1
             row = json.load(row)
             parnet_id = row['parent_id']
@@ -34,3 +49,4 @@ if __name__ == "__main__":
             score = row['score']
             subreddit = row['subreddit']
             
+            parnet_data = find_parent(parnet_id)
